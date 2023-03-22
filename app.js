69d14838a0772afb09c527c15e22954efb244e7b
Oluwatosin Efereyan <tosin.developer@gmail.com>
2023-03-22 21:09:34 +0000

    Add jQuery

---
+++ app.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+	$("button").on('click', function() {
+		const newTaskDescription = $("#newTask").val();
+		tasks.push(newTaskDescription);
+		localStorage.setItem("tasks", JSON.stringify(tasks))
+		$("#newTask").val("")
+
+		showTasks()
+	});
+
+	$(".alertButton").on('click', function() {
+	alert("Task Done")
+
+	})
+	
+	function showTasks() {
+		const getTasks = localStorage.getItem("tasks")
+		let myTasks = JSON.parse(getTasks)
+
+		let myTasksList = "";
+
+		myTasks.forEach(function(task) {
+			myTasksList += `
+				<li>
+					${task}
+					<button type="button" class="alertButton">Completed</button>
+				</li>
+			`
+		})
+
+		$("#my_tasks").html(myTasksList);
+	}
+
+	const tasks = [];
+
+	localStorage.setItem("tasks", JSON.stringify(tasks))
+
+	// showTasks()
+});
+
+
